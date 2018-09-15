722f2c2cf3c56842bbbe84d5a0cbbbaaeecd7006
Eiji Onchi <onchi.eiji@gmail.com>
2018-09-15 19:41:21 +0900

    Moved Axis to its own file

--- canvas/axes.go
+++ canvas/axes.go
@@ -3,7 +3,6 @@ package canvas
 import (
 	"fmt"
 	"image"
-	"image/color"
 	"image/draw"
 	"log"
 
@@ -215,106 +214,4 @@ func border(dst draw.Image, r image.Rectangle, w int, src image.Image,
 func (ax *Axes) Render(dst draw.Image) {
 	ax.Primitive.Render(dst)
 	border(dst, ax.Bounds(), -2, &image.Uniform{colornames.Black}, image.ZP, draw.Src)
-}
-
-// Axis represents a Primitive for horizontal and vertical axes with Axes as its parent.
-type Axis struct {
-	Primitive
-	W        int
-	Min, Max float64
-	Type     byte
-	Parent   *Axes
-	Typer    *Typer
-}
-
-// NewAxis creates a new Axis linked to an Axes.
-// which = 0 defines the Axis as horizontal.
-// which = 1 defines the Axis as vertical.
-func NewAxis(parent *Axes, which byte) (*Axis, error) {
-	var ax Axis
-	var o, s [2]float64
-
-	switch which {
-	case 0:
-		o = [2]float64{0, -0.1}
-		s = [2]float64{1, 0.2}
-	case 1:
-		o = [2]float64{-0.05, 0}
-		s = [2]float64{0.1, 1}
-	}
-
-	ax.W = 2
-
-	ax.Parent = parent
-	ax.Type = which
-	ax.Origin = o
-	ax.Size = s
-	Tc := mat.NewDense(3, 3, []float64{
-		s[0], 0, o[0],
-		0, s[1], o[1],
-		0, 0, 1,
-	})
-	ax.T = append(ax.T, parent.T...)
-	ax.T = append(ax.T, Tc)
-	ax.FillColor = color.Transparent
-
-	parent.Children = append(parent.Children, &ax)
-	return &ax, nil
-}
-
-// Render creates a Typer to be used by the children Labels.
-// The size of Typer is calculated whenever Axis is requested to render.
-// This ensures the size is updated on any parent's change.
-func (a *Axis) Render(dst draw.Image) {
-	if len(a.Children) == 0 {
-		return
-	}
-	l := a.Children[0].(*Label)
-	bounds := l.Bounds()
-	height := bounds.Max.Y - bounds.Min.Y
-	t, _ := NewTyper(height * 72 / 300)
-	t.XAlign = l.XAlign
-	t.YAlign = l.YAlign
-	a.Typer = t
-}
-
-// Labels adds X labels to the Axis with regular spacing.
-func (a *Axis) Labels(X []string, padding float64) {
-	var spacing = (1 - padding*2) / (float64(len(X)) - 1)
-
-	switch a.Type {
-	case 0:
-		for i := range X {
-			l, _ := NewLabel(a, padding+spacing*float64(i), a.Size[0]*(0.4), 0.5, X[i])
-			l.YAlign = TopAlign
-		}
-	case 1:
-		for i := range X {
-			l, _ := NewLabel(a, a.Size[1]*(0.4), padding+spacing*float64(i), 0.1, X[i])
-			l.XAlign = RightAlign
-		}
-	}
-}
-
-func (a *Axis) bounds() image.Rectangle {
-	var x0, y0, x1, y1 int
-
-	v := transform(a)
-
-	x0 = int(v.At(0, 0))
-	y0 = int(v.At(1, 0))
-	x1 = int(v.At(0, 1))
-	y1 = int(v.At(1, 1))
-
-	if x0 == x1 {
-		x0 -= a.W
-		y0 += a.W * 2
-	}
-	if y0 == y1 {
-		y1 += a.W
-		x0 -= a.W * 2
-	}
-
-	//return image.Rect(min(x0, x1), min(y0, y1), max(x0, x1), max(y0, y1))
-	return a.Primitive.Bounds()
 }
