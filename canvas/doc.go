c9a9391b58d1f01f12ce1a7ecee6a2d0cfe569a9
Eiji Onchi <onchi.eiji@gmail.com>
2018-09-17 17:40:49 +0900

    Updated documentation

--- canvas/doc.go
+++ canvas/doc.go
@@ -1,6 +1,18 @@
 // Package canvas holds all the components required to plot a chart.
-// It can be imported as:
-//  import github.com/cgxeiji/plt/canvas
+//
+// All canvas elements except Figure depend on a parent.
+// To create a figure, use the method:
+//  fig, err := canvas.NewFigure(w, h)
+//  if err != nil {
+//		panic(err)
+//  }
+//
+// All other elements should be created from a parent
+// as shown in this family tree:
+//  Figure
+//   |- Axes (figure.NewAxes(), figure.SubAxes(c, r))
+//       |- Bar Chart (axes.BarPlot(X, Y))
+//       |- Scatter Point Chart (axes.ScatterPlot(X, Y))
 //
 // Canvas uses a primitive as the building block of the plotter.
 // A primitive implements Container and holds all the information
